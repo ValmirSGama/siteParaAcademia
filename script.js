894ce258b263555c634eb7c6df41e3f723c26bd0
Valmir Sales <valmir_salesgama@hotmail.com>
2024-11-18 16:37:00 -0300

    Adição da seção about, estilização e responsividade

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ menu.onclick = () =>{
     navbar.classList.toggle('active')
 }
 
+window.onscroll = () =>{
+    menu.classList.remove('fa-times')
+    navbar.classList.remove('active')
+}
+
 var swiper = new Swiper(".home-slider", {
     spaceBetween: 20,
     effect: "fade",
